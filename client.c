b197ee2c031e74da9e9f753ca6314692b06c0db8
Lourenço António Dala <85746261+LouAntonio@users.noreply.github.com>
2024-07-30 14:02:16 +0000

    its alive!

--- client.c
+++ client.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
 
-void	send_signal(unsigned char c){
+void	send_signal(int pid, unsigned char c){
 	int i = 8;
 	int 	bit;
 	int res = 0;
@@ -10,19 +11,19 @@ void	send_signal(unsigned char c){
 	while (i--)
 	{
 		if (((c >> i) & 1))
-			res = res * 2 + 1;
+			kill(pid, SIGUSR1);
 		else
-			res = res * 2;
-		bit = ((c >> i) & 1) + '0';
-		write(1, &bit, 1);
+			kill(pid, SIGUSR2);
+		usleep(350);
 	}
-	write(1, &res, 1);
-	write(1, "\n", 1);
+
 }
 
 int	main(int ac, char **av){
+	int pid = atoi(av[2]);
 	int i = 0;
 	char *str = av[1];
-	while (str[i])
-		send_signal(str[i++]);
+	while (str[i]){
+		send_signal(pid, str[i++]);
+	}
 }
--- server.c
+++ server.c
@@ -1,15 +1,33 @@
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
+
+void  stoc(int bit)
+{
+	static int i = 0;
+	static int c = 0;
+
+	c = c * 2;
+	if (bit == SIGUSR1)
+		c = c + 1;
+	i++;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
+}
 
 int main() 
 {
-  pid_t pid = getpid();
-  printf("Server PID = %d\n", pid);
-  
-  signal(SIGINT, signal_handler);
-  signal(SIGTERM, signal_handler);
+pid_t pid = getpid();
+printf("Server PID = %d\n", pid);
 
-  return 0;
+signal(SIGUSR1, stoc);
+signal(SIGUSR2, stoc);
+pause();
+while (1);
+
+return 0;
 }
